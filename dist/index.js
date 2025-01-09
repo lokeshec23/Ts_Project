e5f6122bbe2cf444e8760cb820ccba8506b7ac85
Lokesh_Saravanan <Lokesh.Saravanan@analyticbrains.com>
2025-01-09 20:13:44 +0530

    add age adjustment logic and enable source maps

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
 console.log("Hello, world!");
 let age = 20;
+if (age < 10) {
+    age += 20;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 console.log("Hello, world!");
 let age: number = 20;
+if (age < 10) {
+  age += 20;
+}
+console.log(age);
